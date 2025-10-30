1f6931ee12f0c0b2813b481ffa10c650a045b463
Aymen touati brahim <touatibrahimaymen@gmail.com>
2025-10-30 18:39:19 +0100

    Commit pro mul_god List

--- crs/min_pro.py
+++ crs/min_pro.py
@@ -3,3 +3,10 @@ def sub(a,b):
     return a-b
 def mul(a,b):
     return  a*b
+def sum_list(num):
+    if len(num) == 0:
+        return 0
+    k = 0
+    for n in num:
+        k +=n
+    return k
--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,4 @@
-from crs.min_pro import sub , mul
+from crs.min_pro import sub , mul , sum_list
 
 def test_sub():
     assert sub(8,3) == 5
@@ -10,3 +10,27 @@ def test_mul():
     assert mul(3,6) == 18
 def test_mul01():
     assert mul(3,4) == 12
+
+def test_add_list():
+    assert sum_list([1,5,4]) ==10
+
+def test_add_list02():
+    assert sum_list([1,5,7]) ==13
+
+def test_add_list03():
+    assert sum_list([1,5,4]) ==10
+
+def test_add_list04():
+    assert sum_list([]) ==0
+
+
+
+
+
+
+
+
+
+
+
+
