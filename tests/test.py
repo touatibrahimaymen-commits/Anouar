7a6eb55ebdb8f9fbff4455321e13aa075aa0ca38
Aymen touati brahim <touatibrahimaymen@gmail.com>
2025-10-30 17:16:05 +0100

    Commit pro mul

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,12 @@
-from crs.min_pro import sub
+from crs.min_pro import sub , mul
 
 def test_sub():
     assert sub(8,3) == 5
 
 def test_sub02():
     assert sub(11,6) == 5
+
+def test_mul():
+    assert mul(3,6) == 18
+def test_mul01():
+    assert mul(3,4) == 12
